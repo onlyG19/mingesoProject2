4b15bf1d9504db7c21e9f11580537ad42ce03aa6
onlyG19 <byron.gracia@usach.cl>
2024-05-16 00:14:44 -0400

    initial commit ms-reportes

---
+++ ms-reportes/src/test/java/com/example/msreportes/MsReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msreportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
