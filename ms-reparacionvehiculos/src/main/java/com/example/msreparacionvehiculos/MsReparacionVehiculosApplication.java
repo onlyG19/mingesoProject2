1eb2e5b440d894ac51c8c8d55bbbc2e69a11765c
onlyG19 <byron.gracia@usach.cl>
2024-05-16 00:27:20 -0400

    initial commit ms-reparacionvehiculos

---
+++ ms-reparacionvehiculos/src/main/java/com/example/msreparacionvehiculos/MsReparacionVehiculosApplication.java
@@ -0,0 +1,12 @@
+package com.example.msreparacionvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReparacionVehiculosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsReparacionVehiculosApplication.class, args);
+	}
+
+}
---
+++ ms-reparacionvehiculos/src/test/java/com/example/msreparacionvehiculos/MsReparacionVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msreparacionvehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReparacionVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
