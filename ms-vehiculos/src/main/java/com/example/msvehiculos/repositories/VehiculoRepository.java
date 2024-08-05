0245555e34164c380f040099b7e3061d02a5d492
onlyG19 <byron.gracia@usach.cl>
2024-08-05 19:54:20 -0400

    Eliminados los primeros microservicios ms-reparacionvehiculos, ms-reportes y ms-vehiculos

--- ms-vehiculos/src/main/java/com/example/msvehiculos/repositories/VehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.msvehiculos.repositories;
-
-import com.example.msvehiculos.entities.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
-}
